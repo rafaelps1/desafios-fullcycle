2865c4674f88a5289df6ac08294779fca18fa0de
Rafael Pereira <rafaelps@dcomp.ufs.br>
2023-05-19 19:41:21 -0300

    Add person

--- nginx-with-node/node/index.js
+++ nginx-with-node/node/index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
-    res.send('<h1>Full Cycle Rocks!</h1>')
-})
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+app.get('/', async (req, res) => {
+    let person = await prisma.people.create({
+    data: { name: 'Rafael' },
+    })
+
+    let people = await prisma.people.findMany({
+        select: { name: true }
+    })
+
+    let html = '<h1>Full Cycle Rocks!</h1>'
+    for(let pers of people) {
+        html += `<h5>${pers.name}</h5>`
+    }
+    res.send(html)
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+});
